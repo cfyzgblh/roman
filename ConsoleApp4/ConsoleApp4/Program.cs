5419f88bbbc9c9218aeac3235f3367a20813f684
cfyzgblh <cU2roma@yandex.ru>
2025-08-23 21:27:52 +0300

    4525

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Specialized;
+using System.Text.Json.Serialization;
+
+public class Person
+{
+    public string Name;
+    public string SerName;
+    public string Address;
+    public string Email;
+    public int DateOfBerth;
+
+    public Person (string name, string serName, string address, string email, int dateOfBerth)
+    {
+        Name = name;
+        SerName = serName;
+        Address = address;
+        Email = email;
+        DateOfBerth = dateOfBerth;
+    }   
+}
+
+public class Prodram
+{
+    public static void Main()
+    {
+        Person person = new Person("Рома", "Кузьмин", "40 лет октября 5а", "cU2roma@yandex.ru", 23052005);
+        string Json = JsonSeriaizer.Serialize(person);
+    }
+    
+}
