465bb6d58bae1cb709055c388b40fdf9f168d18a
cfyzgblh <cU2roma@yandex.ru>
2025-08-30 21:19:21 +0300

    Update Program.cs

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -1,40 +1,72 @@
-﻿internal class Prodram
+﻿internal class Program
 {
-    public static void Main()
+    public static readonly object lockObj = new object();
+    public static int posCar1 = 0;
+    public static int posCar2 = 0;
+    public static Thread? thread1;
+    public static Thread? thread2;
+    static void Main()
     {
-        Car Yaz = new Car("Уаз", "Буханка", "1", "ЗМЗ - 409");
-        Car Lada = new Car("Лада", "Приора", "2", "ВАЗ - 211127");
-        Thread threadYas = new Thread(Yaz.Go);
-        Thread threadLada = new Thread(Lada.Go);
-        threadYas.Start();
-        threadLada.Start();
-        threadYas.Join();
-        threadLada.Join();
+        //ParameterizedThreadStart race = new ParameterizedThreadStart(?Race);
+        Car car1 = new("Ламба");
+        Car car2 = new("Жигуль");
+        //thread1 = new Thread(race);
+        //thread2 = new Thread(race);
+        thread1 = new Thread(() => Race(car1));
+        thread2 = new Thread(() => Race(car2));
+        thread1.Start();
+        thread2.Start();
+        thread1.Join();
+        thread2.Join();
+        Console.WriteLine("Гонка завершена!");
 
     }
-}
-public class Car
-{
-    private static readonly object lockObject = new object();
-    public string Brand;
-    public string Seria;
-    public string Model;
-    public string Engine;
-
-    public Car(string brand, string seria, string model, string engine)
-    {
-        Brand = brand;
-        Seria = seria;
-        Model = model;
-        Engine = engine;
-    }
-
-    public void Go()
-    {
-        for (int i = 0; i < 100; i++)
+    public static void Race(Car car)
+    { 
+        //Car car = (Car)carObject;
+        for (int i = 0; i < 20; i++)
         {
-            Console.WriteLine($"Скорость автомобиля {Brand} = {i}")  ;
-            Thread.Sleep(1000);
+
+           
+            
+                if (Thread.CurrentThread.ManagedThreadId == thread1.ManagedThreadId)
+                {
+                    while (posCar1 > posCar2) { }
+                    lock (lockObj)
+                    {
+                        car.Move();
+                        posCar1++;
+                        Console.SetCursorPosition(0, i);
+                        Random random = new Random();
+                        Thread.Sleep(random.Next(1, 10));
+                        Console.WriteLine($"{car.Name} - {car.Position}");
+                    }
+                }
+                else if (Thread.CurrentThread.ManagedThreadId == thread2.ManagedThreadId)
+                {
+                    while (posCar2 > posCar1) { }
+                    lock (lockObj)
+                    {
+                        car.Move();
+                        posCar2++;
+                        Console.SetCursorPosition(20, i);
+                        Random random = new Random();
+                        Thread.Sleep(random.Next(1, 10));
+                        Console.WriteLine($"{car.Name} - {car.Position}");
+                    }
+                }
+            
         }
     }
+}
+
+public class Car
+{
+    public int Position { get; private set; } = 0;
+    public string? Name { get; set; }
+    public Car(string name)
+    {
+        Name = name;
+    }
+    public void Move() => Position++;
 }
