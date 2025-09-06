a0d77272b74bcc9a36adc7fa40ad0e1b67199ffe
cfyzgblh <cU2roma@yandex.ru>
2025-09-06 20:27:21 +0300

    Create Program.cs

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Diagnostics;
+string puh = "C:\\Users\\Student\\source\\repos\\roman\\txt.txt";
+ProcessStartInfo procInfo = new ProcessStartInfo();
+procInfo.Arguments = "C:\\Users\\Student\\source\\repos\\roman\\txt.txt";
+
+for (int i =0 ; i < 5; i++)
+{
+    File.AppendAllText(puh ,"МАрсияне привет");
+    Thread.Sleep(1000);
+}
