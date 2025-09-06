1f1314839305b952608ffce87715b0467c58c084
cfyzgblh <cU2roma@yandex.ru>
2025-09-06 21:31:29 +0300

    asinc

--- ConsoleApp8/ConsoleApp8/Program.cs
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -44,5 +44,30 @@
 //        Console.WriteLine(factorial.ToString());
 //        return factorial;
 //    }
-    
-//}
+
+
+internal class Program
+{
+    public static async Task Main()
+    {
+        int result = await GetNumber(10, 5);
+        Console.WriteLine(result);
+    }
+    public static Task<int> GetNumber(int a, int b)
+    {
+        return Task.Run(() =>
+        {
+            while (b != 0)
+            {
+                int temp = b; // Временная переменная для хранения текущего b
+                b = a % b;   // Находим остаток от деления a на b
+                a = temp;    // Теперь a становится предыдущим значением b
+            }
+
+         
+            return a; // Когда b становится 0, a - это и есть НОД
+            
+        });
+        
+}
+}
