82f3ca17c6f6acec45a995ed5f9a9e9775fad84f
cfyzgblh <cU2roma@yandex.ru>
2025-11-01 21:40:22 +0300

    123

---
+++ WpfApp7/WpfApp7/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp7
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Sername { get; set; }
+        public int Age { get; set; }
+
+        public User(string name, string sername, int age)
+        {
+            Name = name;
+            Sername = sername;
+            Age = age;
+        }
+    }
+}
